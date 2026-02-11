5f7892c45146b27230fea1a8d0e00cd76d08ce76
Ahmed-Elgammal13 <stefishot@signinid.com>
2026-02-11 14:09:00 -0600

    Simplify fetch function by removing logging and headers

--- _worker.js
+++ _worker.js
@@ -1,16 +1,5 @@
-// _worker.js - This runs on Cloudflare's edge
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    
-    // Example: Log all visits
-    console.log(`Visitor from ${request.cf?.country} to ${url.pathname}`);
-    
-    // Example: Add custom headers
-    const response = await env.ASSETS.fetch(request);
-    const newResponse = new Response(response.body, response);
-    newResponse.headers.set('X-Custom-Header', 'MySite');
-    
-    return newResponse;
+    return env.ASSETS.fetch(request);
   }
 };
